d2a0f04cffce156ec315c51e8ef2a121a20d3e61
Vuk Kapor <vukkapor12@gmail.com>
2019-07-05 18:31:23 +0200

    All posts

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -1,7 +1,30 @@
 <template>
-  <div></div>
+  <div style="text-align: center">
+    <div v-for="post in posts" :key="post.id">
+      <h1>{{post.title}}</h1>
+      <p>{{post.text}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { postsService } from "../services/posts";
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+
+  created() {
+    postsService
+      .getAll()
+      .then(response => {
+        this.posts = response.data;
+      })
+      .catch(e => {
+        alert(e);
+      });
+  }
+};
 </script>
---
+++ src/services/posts.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export default class PostsService {
+    constructor() {
+        axios.defaults.baseURL = 'http://localhost:3000/api/'
+    }
+
+    getAll() {
+        return axios.get('posts')
+    }
+}
+
+export const postsService = new PostsService()
